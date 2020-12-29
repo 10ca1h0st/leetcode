c1b93fa2f1520bc287ee8483a34909669b7d8bb2
wujiaming <1114802868@qq.com>
2020-12-29 09:00:34 +0800

    from official answer

---
+++ java/No330-按要求补齐数组.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minPatches(int[] nums, int n) {
+        int patches = 0;
+        long x = 1;
+        int length = nums.length, index = 0;
+        while (x <= n) {
+            if (index < length && nums[index] <= x) {
+                x += nums[index];
+                index++;
+            } else {
+                x *= 2;
+                patches++;
+            }
+        }
+        return patches;
+    }
+}
