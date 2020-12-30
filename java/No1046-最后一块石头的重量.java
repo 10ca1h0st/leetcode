9d31e535f1ab56f0fa67fda8cb7b73595dbeb6d6
wujiaming <1114802868@qq.com>
2020-12-30 08:18:32 +0800

    使用堆

---
+++ java/No1046-最后一块石头的重量.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> heap = new PriorityQueue<>(new Comparator<Integer>() {
+            public int compare(Integer a, Integer b) {
+                if(a > b) {
+                    return -1;
+                }
+                return 1;
+            }
+        });
+        for(int stone : stones) {
+            heap.add(stone);
+        }
+        int a = 0;
+        int b = 0;
+        while(!heap.isEmpty()) {
+            a = heap.poll();
+            if(heap.isEmpty()) {
+                return a;
+            }
+            b = heap.poll();
+            if(a == b) {
+                continue;
+            }
+            else if(a > b) {
+                heap.add(a-b);
+            }
+            else {
+                heap.add(b-a);
+            }
+        }
+        return 0;
+    }
+}
