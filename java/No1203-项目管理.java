b368fe9ae642843e33d3f5fe25064a82c73a8d35
wujiaming <1114802868@qq.com>
2021-01-12 09:24:51 +0800

    拓扑排序

---
+++ java/No1203-项目管理.java
@@ -0,0 +1,95 @@
+class Solution {
+    public int[] sortItems(int n, int m, int[] group, List<List<Integer>> beforeItems) {
+        List<List<Integer>> groupItem = new ArrayList<List<Integer>>();
+        for (int i = 0; i < n + m; ++i) {
+            groupItem.add(new ArrayList<Integer>());
+        }
+
+        // 组间和组内依赖图
+        List<List<Integer>> groupGraph = new ArrayList<List<Integer>>();
+        for (int i = 0; i < n + m; ++i) {
+            groupGraph.add(new ArrayList<Integer>());
+        }
+        List<List<Integer>> itemGraph = new ArrayList<List<Integer>>();
+        for (int i = 0; i < n; ++i) {
+            itemGraph.add(new ArrayList<Integer>());
+        }
+
+        // 组间和组内入度数组
+        int[] groupDegree = new int[n + m];
+        int[] itemDegree = new int[n];
+        
+        List<Integer> id = new ArrayList<Integer>();
+        for (int i = 0; i < n + m; ++i) {
+            id.add(i);
+        }
+
+        int leftId = m;
+        // 给未分配的 item 分配一个 groupId
+        for (int i = 0; i < n; ++i) {
+            if (group[i] == -1) {
+                group[i] = leftId;
+                leftId += 1;
+            }
+            groupItem.get(group[i]).add(i);
+        }
+        // 依赖关系建图
+        for (int i = 0; i < n; ++i) {
+            int curGroupId = group[i];
+            for (int item : beforeItems.get(i)) {
+                int beforeGroupId = group[item];
+                if (beforeGroupId == curGroupId) {
+                    itemDegree[i] += 1;
+                    itemGraph.get(item).add(i);   
+                } else {
+                    groupDegree[curGroupId] += 1;
+                    groupGraph.get(beforeGroupId).add(curGroupId);
+                }
+            }
+        }
+
+        // 组间拓扑关系排序
+        List<Integer> groupTopSort = topSort(groupDegree, groupGraph, id); 
+        if (groupTopSort.size() == 0) {
+            return new int[0];
+        }
+        int[] ans = new int[n];
+        int index = 0;
+        // 组内拓扑关系排序
+        for (int curGroupId : groupTopSort) {
+            int size = groupItem.get(curGroupId).size();
+            if (size == 0) {
+                continue;
+            }
+            List<Integer> res = topSort(itemDegree, itemGraph, groupItem.get(curGroupId));
+            if (res.size() == 0) {
+                return new int[0];
+            }
+            for (int item : res) {
+                ans[index++] = item;
+            }
+        }
+        return ans;
+    }
+
+    public List<Integer> topSort(int[] deg, List<List<Integer>> graph, List<Integer> items) {
+        Queue<Integer> queue = new LinkedList<Integer>();
+        for (int item : items) {
+            if (deg[item] == 0) {
+                queue.offer(item);
+            }
+        }
+        List<Integer> res = new ArrayList<Integer>();
+        while (!queue.isEmpty()) {
+            int u = queue.poll(); 
+            res.add(u);
+            for (int v : graph.get(u)) {
+                if (--deg[v] == 0) {
+                    queue.offer(v);
+                }
+            }
+        }
+        return res.size() == items.size() ? res : new ArrayList<Integer>();
+    }
+}
+
