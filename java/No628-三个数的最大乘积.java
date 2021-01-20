77e5f6c1df6091809f83e457d6f35d0beadcd051
wujiaming <1114802868@qq.com>
2021-01-20 08:11:20 +0800

    简单题竟然没做出来

---
+++ java/No628-三个数的最大乘积.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int maximumProduct(int[] nums) {
+        // 最小的和第二小的
+        int min1 = Integer.MAX_VALUE, min2 = Integer.MAX_VALUE;
+        // 最大的、第二大的和第三大的
+        int max1 = Integer.MIN_VALUE, max2 = Integer.MIN_VALUE, max3 = Integer.MIN_VALUE;
+
+        for (int x : nums) {
+            if (x < min1) {
+                min2 = min1;
+                min1 = x;
+            } else if (x < min2) {
+                min2 = x;
+            }
+
+            if (x > max1) {
+                max3 = max2;
+                max2 = max1;
+                max1 = x;
+            } else if (x > max2) {
+                max3 = max2;
+                max2 = x;
+            } else if (x > max3) {
+                max3 = x;
+            }
+        }
+
+        return Math.max(min1 * min2 * max1, max1 * max2 * max3);
+    }
+}
