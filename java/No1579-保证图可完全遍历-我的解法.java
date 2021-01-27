1072859dd27c8b1d5d0717b045edd49854d96f61
wujiaming <1114802868@qq.com>
2021-01-28 05:59:56 +0800

    1579题我的解法，但是只通过了24个样例，没想明白为什么

---
+++ java/No1579-保证图可完全遍历-我的解法.java
@@ -0,0 +1,130 @@
+class Solution {
+    static Integer AliceType = 1;
+    static Integer BobType = 2;
+    static Integer BIG = 10;
+    static Integer SMALL = 1;
+    // 最小生成树存放的是边的索引
+    Set<Integer> mst;
+    Set<Integer> deleted;
+
+    public int maxNumEdgesToRemove(int n, int[][] edges) {
+        mst = new HashSet<>();
+        deleted = new HashSet<>();
+        for(int[] edge : edges) {
+            edge[1]--;
+            edge[2]--;
+        }
+        // 新的边，索引0为原来边在edges中的索引，索引4为边的权重
+        int[][] newEdges = new int[edges.length][5];
+        for(int i = 0; i < newEdges.length; i++) {
+            newEdges[i][0] = i;
+            newEdges[i][1] = edges[i][0];
+            newEdges[i][2] = edges[i][1];
+            newEdges[i][3] = edges[i][2];
+            if(edges[i][0] == AliceType || edges[i][0] == BobType) {
+                // 只能单独被一人使用的边，权重设置为大
+                newEdges[i][4] = BIG;
+            }
+            else {
+                newEdges[i][4] = SMALL;
+            }
+        }
+        // 先判断Alice是否可以完全遍历图
+        boolean canAlice = isConnected(n, newEdges, new HashSet<Integer>(List.of(BobType)));
+        if(!canAlice) {
+            return -1;
+        }
+
+        boolean canBob = isConnected(n, newEdges, new HashSet<Integer>(List.of(AliceType)));
+        if(!canBob) {
+            return -1;
+        }
+        genMST(n, newEdges, new HashSet<Integer>(List.of(BobType)));
+        genMST(n, newEdges, new HashSet<Integer>(List.of(AliceType)));
+        System.out.println("edges length is "+edges.length);
+        System.out.println("mst size is "+mst.size());
+        // return newEdges.length - mst.size();
+        for(int index : mst) {
+            System.out.print(edges[index][0]+" "+edges[index][1]+" "+edges[index][2]+"$");
+        }
+        return deleted.size();
+    }
+
+    boolean isConnected(int n, int[][] edges, Set<Integer> filter) {
+        UnionFind uf = new UnionFind(n);
+        for(int[] edge : edges) {
+            if(filter.contains(edge[1])) {
+                continue;
+            }
+            uf.union(edge[2], edge[3]);
+        }
+        if(uf.countCC != 1) {
+            return false;
+        }
+        return true;
+    }
+
+    void genMST(int n, int[][] edges, Set<Integer> filter) {
+        UnionFind uf = new UnionFind(n);
+        PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>() {
+            @Override
+            public int compare(int[] a, int[] b) {
+                return a[4] - b[4];
+            }
+        });
+        for(int[] edge : edges) {
+            if(filter.contains(edge[1])) {
+                continue;
+            }
+            pq.add(edge);
+        }
+        int size = 0;
+        // while(!pq.isEmpty() && size < n-1) {
+        while(!pq.isEmpty()) {
+            int[] edge = pq.poll();
+            // System.out.println("edge type is "+edge[1]);
+            if(uf.union(edge[2], edge[3])) {
+                size++;
+                mst.add(edge[0]);
+            }
+            else {
+                // System.out.println("find a useless edge");
+                deleted.add(edge[0]);
+            }
+        }
+        // System.out.println(uf.countCC);
+        // System.out.println(size);
+    }
+}
+
+class UnionFind {
+    int[] parent;
+    // 连通分量的个数
+    int countCC;
+
+    UnionFind(int n) {
+        parent = new int[n];
+        for(int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+        countCC = n;
+    }
+
+    boolean union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+        if(rootX == rootY) {
+            return false;
+        }
+        parent[rootY] = rootX;
+        countCC--;
+        return true;
+    }
+
+    int find(int x) {
+        if(x != parent[x]) {
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
+    }
+}
