00439368b09eda0435d11305cfed6b9af620d9c3
wujiaming <1114802868@qq.com>
2021-01-27 10:49:15 +0800

    官方解答，但是我自己使用mst相关知识，只通过了24个测试样例，但我觉得我的代码没错啊

---
+++ java/No1579-保证图可完全遍历.java
@@ -0,0 +1,91 @@
+class Solution {
+    public int maxNumEdgesToRemove(int n, int[][] edges) {
+        UnionFind ufa = new UnionFind(n);
+        UnionFind ufb = new UnionFind(n);
+        int ans = 0;
+
+        // 节点编号改为从 0 开始
+        for (int[] edge : edges) {
+            --edge[1];
+            --edge[2];
+        }
+
+        // 公共边
+        for (int[] edge : edges) {
+            if (edge[0] == 3) {
+                if (!ufa.unite(edge[1], edge[2])) {
+                    ++ans;
+                } else {
+                    ufb.unite(edge[1], edge[2]);
+                }
+            }
+        }
+
+        // 独占边
+        for (int[] edge : edges) {
+            if (edge[0] == 1) {
+                // Alice 独占边
+                if (!ufa.unite(edge[1], edge[2])) {
+                    ++ans;
+                }
+            } else if (edge[0] == 2) {
+                // Bob 独占边
+                if (!ufb.unite(edge[1], edge[2])) {
+                    ++ans;
+                }
+            }
+        }
+
+        if (ufa.setCount != 1 || ufb.setCount != 1) {
+            return -1;
+        }
+        return ans;
+    }
+}
+
+// 并查集模板
+class UnionFind {
+    int[] parent;
+    int[] size;
+    int n;
+    // 当前连通分量数目
+    int setCount;
+
+    public UnionFind(int n) {
+        this.n = n;
+        this.setCount = n;
+        this.parent = new int[n];
+        this.size = new int[n];
+        Arrays.fill(size, 1);
+        for (int i = 0; i < n; ++i) {
+            parent[i] = i;
+        }
+    }
+    
+    public int findset(int x) {
+        return parent[x] == x ? x : (parent[x] = findset(parent[x]));
+    }
+    
+    public boolean unite(int x, int y) {
+        x = findset(x);
+        y = findset(y);
+        if (x == y) {
+            return false;
+        }
+        if (size[x] < size[y]) {
+            int temp = x;
+            x = y;
+            y = temp;
+        }
+        parent[y] = x;
+        size[x] += size[y];
+        --setCount;
+        return true;
+    }
+    
+    public boolean connected(int x, int y) {
+        x = findset(x);
+        y = findset(y);
+        return x == y;
+    }
+}
