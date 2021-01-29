84cbf25e9777af1172b764136ca486ade5042c70
wujiaming <1114802868@qq.com>
2021-01-29 11:04:46 +0800

    官方解答，看似使用dp，结果使用并查集，其中包含了并查集模板

---
+++ java/No1631-最小体力消耗路径.java
@@ -0,0 +1,87 @@
+class Solution {
+    public int minimumEffortPath(int[][] heights) {
+        int m = heights.length;
+        int n = heights[0].length;
+        List<int[]> edges = new ArrayList<int[]>();
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                int id = i * n + j;
+                if (i > 0) {
+                    edges.add(new int[]{id - n, id, Math.abs(heights[i][j] - heights[i - 1][j])});
+                }
+                if (j > 0) {
+                    edges.add(new int[]{id - 1, id, Math.abs(heights[i][j] - heights[i][j - 1])});
+                }
+            }
+        }
+        Collections.sort(edges, new Comparator<int[]>() {
+            public int compare(int[] edge1, int[] edge2) {
+                return edge1[2] - edge2[2];
+            }
+        });
+
+        UnionFind uf = new UnionFind(m * n);
+        int ans = 0;
+        for (int[] edge : edges) {
+            int x = edge[0], y = edge[1], v = edge[2];
+            uf.unite(x, y);
+            if (uf.connected(0, m * n - 1)) {
+                ans = v;
+                break;
+            }
+        }
+        return ans;
+    }
+}
+
+// 并查集模板
+class UnionFind {
+    int[] parent;
+    int[] size;
+    int n;
+    // 当前连通分量数目
+    int setCount;
+
+    public UnionFind(int n) {
+        this.n = n;
+        this.setCount = n;
+        this.parent = new int[n];
+        this.size = new int[n];
+        Arrays.fill(size, 1);
+        for (int i = 0; i < n; ++i) {
+            parent[i] = i;
+        }
+    }
+    
+    public int findset(int x) {
+        return parent[x] == x ? x : (parent[x] = findset(parent[x]));
+    }
+    
+    public boolean unite(int x, int y) {
+        x = findset(x);
+        y = findset(y);
+        if (x == y) {
+            return false;
+        }
+        if (size[x] < size[y]) {
+            int temp = x;
+            x = y;
+            y = temp;
+        }
+        parent[y] = x;
+        size[x] += size[y];
+        --setCount;
+        return true;
+    }
+    
+    public boolean connected(int x, int y) {
+        x = findset(x);
+        y = findset(y);
+        return x == y;
+    }
+}
+
+作者：LeetCode-Solution
+链接：https://leetcode-cn.com/problems/path-with-minimum-effort/solution/zui-xiao-ti-li-xiao-hao-lu-jing-by-leetc-3q2j/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
