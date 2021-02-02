dcbd8b935fcdf94a070540aa98172ac1d0f0b55b
wujiaming <1114802868@qq.com>
2021-02-02 20:03:16 +0800

    添加了一个自己写的，使用最朴素的滑动窗口的版本

--- java/No424-替换后的最长重复字符.java
+++ java/No424-替换后的最长重复字符.java
@@ -16,3 +16,33 @@ class Solution {
         return right - left;
     }
 }
+
+// 自己写的使用滑动窗口，较慢的版本
+class Solution {
+    public int characterReplacement(String s, int k) {
+        // 区间包含left，不包含right，区间表示该区间可以通过替换，变成重复子串
+        int left = 0;
+        int right = 0;
+        // 在区间中的字母出现的次数
+        int[] num = new int[26];
+        int maxNum = 0;
+        char[] chs = s.toCharArray();
+        // 包含重复字母的最长子串长度
+        int ret = 0;
+        while(right < chs.length) {
+            char ch = chs[right];
+            right++;
+            // 现在的区间为[left, right)
+            num[ch-'A']++;
+            maxNum = Arrays.stream(num).max().getAsInt();
+            while(right - left > maxNum + k) {
+                num[chs[left]-'A']--;
+                left++;
+                maxNum = Arrays.stream(num).max().getAsInt();
+            }
+            ret = Math.max(ret, right - left);
+        }
+        return ret;
+    }
+}
+
