2ebc5f2125c97f6f7686073e5b39a580c0feef48
wujiaming <1114802868@qq.com>
2021-02-07 09:46:57 +0800

    简单题，但是参考了官方题解

---
+++ java/No665-非递减数列.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean checkPossibility(int[] nums) {
+        int n = nums.length;
+        int cnt = 0;
+        for (int i = 0; i < n - 1; ++i) {
+            int x = nums[i], y = nums[i + 1];
+            if (x > y) {
+                cnt++;
+                if(cnt > 1) {
+                    return false;
+                }
+                nums[i] = y;
+                if (isSorted(nums)) {
+                    return true;
+                }
+                nums[i] = x; // 复原
+                nums[i + 1] = x;
+                return isSorted(nums);
+            }
+        }
+        return true;
+    }
+
+    public boolean isSorted(int[] nums) {
+        int n = nums.length;
+        for (int i = 1; i < n; ++i) {
+            if (nums[i - 1] > nums[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
