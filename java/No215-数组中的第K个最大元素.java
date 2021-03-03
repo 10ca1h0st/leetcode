07e18a2fdd8d7245e409f65facbfd0d1b263f4e9
wujiaming <1114802868@qq.com>
2021-03-03 16:22:00 +0800

    可以使用快速排序的思想，还可以使用大根堆

--- java/No215-数组中的第K个最大元素.java
+++ java/No215-数组中的第K个最大元素.java
@@ -37,3 +37,97 @@ class Solution {
         nums[j] = temp;
     }
 }
+
+// 使用堆
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        MyPriorityQueue pq = new MyPriorityQueue(nums.length, true);
+        for(int num : nums) {
+            pq.insert(num);
+        }
+        int delCount = k;
+        int ret = 0;
+        while(delCount > 0) {
+            ret = pq.delete();
+            delCount--;
+        }
+        return ret;
+    }
+
+    // 固定大小的优先队列
+    class MyPriorityQueue {
+        int[] arr;
+        // 大根堆？
+        boolean max;
+        // 目前堆中含有的元素数量
+        int N;
+
+        MyPriorityQueue(int length, boolean max) {
+            arr = new int[length];
+            this.max = max;
+            N = 0;
+        }
+
+        void insert(int num) {
+            arr[N] = num;
+            N++;
+            swim(N-1);
+        }
+
+        int delete() {
+            int ret = arr[0];
+            swap(0, N-1);
+            N--;
+            sink(0);
+            return ret;
+        }
+
+        void swap(int i, int j) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+
+        void swim(int i) {
+            int parent = (i-1) / 2;
+            while(i > 0 && less(parent, i)) {
+                swap(parent, i);
+                i = parent;
+                parent = (i-1) / 2;
+            }
+        }
+
+        void sink(int i) {
+            // arr[i]是否需要下沉
+            boolean exch = false;
+            int child = 0;
+            while((child=2*i+1) < N) {
+                exch = false;
+                if(child < N-1 && less(child, child+1)) {
+                    child++;
+                }
+                if(less(i, child)) {
+                    swap(i, child);
+                    exch = true;
+                }
+                if(exch) {
+                    i = child;
+                }
+                else {
+                    break;
+                }
+            }
+        }
+
+        // 如果是大根堆，返回arr[i]<arr[j]
+        // 如果是小根堆，返回arr[i]>arr[j]
+        boolean less(int i, int j) {
+            if(max) {
+                return arr[i] < arr[j];
+            }
+            else {
+                return arr[i] > arr[j];
+            }
+        }
+    }
+}
