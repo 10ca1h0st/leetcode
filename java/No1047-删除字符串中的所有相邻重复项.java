1aad96de4ceffe9940057b4e79714ffc099442c0
wujiaming <1114802868@qq.com>
2021-03-09 08:23:00 +0800

    简单题，我使用双指针，题解使用栈

--- java/No1047-删除字符串中的所有相邻重复项.java
+++ java/No1047-删除字符串中的所有相邻重复项.java
@@ -29,3 +29,27 @@ class Solution {
         return sb.toString();
     }
 }
+
+// 官方解答
+class Solution {
+    public String removeDuplicates(String S) {
+        StringBuffer stack = new StringBuffer();
+        int top = -1;
+        for (int i = 0; i < S.length(); ++i) {
+            char ch = S.charAt(i);
+            if (top >= 0 && stack.charAt(top) == ch) {
+                stack.deleteCharAt(top);
+                --top;
+            } else {
+                stack.append(ch);
+                ++top;
+            }
+        }
+        return stack.toString();
+    }
+}
+
+作者：LeetCode-Solution
+链接：https://leetcode-cn.com/problems/remove-all-adjacent-duplicates-in-string/solution/shan-chu-zi-fu-chuan-zhong-de-suo-you-xi-4ohr/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
