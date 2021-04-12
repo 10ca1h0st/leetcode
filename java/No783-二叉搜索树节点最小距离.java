6d613f5b67a1d12e2aeaeba8d7170f17cff581c4
wujiaming <1114802868@qq.com>
2021-04-13 07:40:06 +0800

    直接利用二叉搜索树中序遍历单调递增的特性

--- java/No783-二叉搜索树节点最小距离.java
+++ java/No783-二叉搜索树节点最小距离.java
@@ -63,3 +63,35 @@ class Solution {
         return ans;
     }
 }
+
+// 官方解答 直接使用中序遍历
+class Solution {
+    int pre;
+    int ans;
+
+    public int minDiffInBST(TreeNode root) {
+        ans = Integer.MAX_VALUE;
+        pre = -1;
+        dfs(root);
+        return ans;
+    }
+
+    public void dfs(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        dfs(root.left);
+        if (pre == -1) {
+            pre = root.val;
+        } else {
+            ans = Math.min(ans, root.val - pre);
+            pre = root.val;
+        }
+        dfs(root.right);
+    }
+}
+
+作者：LeetCode-Solution
+链接：https://leetcode-cn.com/problems/minimum-distance-between-bst-nodes/solution/er-cha-sou-suo-shu-jie-dian-zui-xiao-ju-8u87w/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
