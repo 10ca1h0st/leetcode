629c845b62744246e4f0e8489c311066836f277f
wujiaming <1114802868@qq.com>
2021-04-14 10:58:51 +0800

    前缀树 懒加载机制

--- java/No208-实现Trie(前缀树).java
+++ java/No208-实现Trie(前缀树).java
@@ -54,3 +54,171 @@ class Trie {
  * boolean param_2 = obj.search(word);
  * boolean param_3 = obj.startsWith(prefix);
  */
+ 
+ 
+ // 懒加载机制
+ class Trie {
+
+    static class TrieNode {
+        // length == 26
+        TrieNode[] children;
+
+        // 目前节点保存的值，根节点不保存值
+        String value;
+
+        // 代表是否插入过这个节点
+        boolean isLeaf;
+
+        TrieNode(String v) {
+            children = new TrieNode[26];
+            value = v;
+            isLeaf = true;
+        }
+    }
+
+    TrieNode root;
+
+    private TrieNode helper;
+
+    static final char base = 'a';
+
+    /** Initialize your data structure here. */
+    public Trie() {
+        root = new TrieNode(null);
+    }
+    
+    /** Inserts a word into the trie. */
+    public void insert(String word) {
+        helper = null;
+        int i = _search(word);
+        if(i == -1) {
+            return;
+        }
+        if(i == word.length()) {
+            helper.isLeaf = true;
+            return;
+        }
+        char ch = word.charAt(i);
+        TrieNode next = helper.children[ch-base];
+        if(next == null) {
+            helper.children[ch-base] = new TrieNode(word.substring(i, word.length()));
+            return;
+        }
+        String v = next.value;
+        // 找到word[i:-1]和v的公共前缀，然后从公共前缀处开始，分裂出两个子节点
+        int j = i;
+        int split = 0;
+        for(; j < word.length() && split < v.length(); split++, j++) {
+            if(word.charAt(j) != v.charAt(split)) {
+                break;
+            }
+        }
+        if(j == word.length()) {
+            next.value = v.substring(0, split);
+            next.isLeaf = true;
+            next.children[v.charAt(split)-base] = new TrieNode(v.substring(split, v.length()));
+        }
+        else {
+            next.isLeaf = false;
+            next.value = v.substring(0, split);
+            char child = word.charAt(j);
+            next.children[child-base] = new TrieNode(word.substring(j, word.length()));
+            child = v.charAt(split);
+            next.children[child-base] = new TrieNode(v.substring(split, v.length()));
+        }
+    }
+
+    // 在insert的时候使用，返回不匹配时遍历到的word的索引，且用helper保存不匹配时的TrieNode
+    // helper代表在当前节点的孩子中，找不到匹配word[i:]的孩子
+    // 返回-1代表搜索成功
+    // 返回word.length()时，只需要把helper的isLeaf属性设为true即可
+    private int _search(String word) {
+        int i = 0;
+        char ch = word.charAt(i);
+        TrieNode cur = root;
+        TrieNode next = null;
+        while(i < word.length()) {
+            ch = word.charAt(i);
+            next = cur.children[ch-base];
+            if(next == null) {
+                helper = cur;
+                return i;
+            }
+            String v = next.value;
+            if(i + v.length() > word.length()) {
+                helper = cur;
+                return i;
+            }
+            if(!v.equals(word.substring(i, i+v.length()))) {
+                helper = cur;
+                return i;
+            }
+            i += v.length();
+            cur = next;
+        }
+        if(!cur.isLeaf) {
+            helper = cur;
+            return i;
+        }
+        return -1;
+    }
+    
+    /** Returns if the word is in the trie. */
+    public boolean search(String word) {
+        int i = 0;
+        char ch = word.charAt(i);
+        TrieNode cur = root;
+        TrieNode next = null;
+        while(i < word.length()) {
+            ch = word.charAt(i);
+            next = cur.children[ch-base];
+            if(next == null) {
+                return false;
+            }
+            String v = next.value;
+            if(i + v.length() > word.length()) {
+                return false;
+            }
+            if(!v.equals(word.substring(i, i+v.length()))) {
+                return false;
+            }
+            i += v.length();
+            cur = next;
+        }
+        if(!cur.isLeaf) {
+            return false;
+        }
+        return true;
+    }
+    
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    public boolean startsWith(String word) {
+        int i = 0;
+        char ch = word.charAt(i);
+        TrieNode cur = root;
+        TrieNode next = null;
+        while(i < word.length()) {
+            ch = word.charAt(i);
+            next = cur.children[ch-base];
+            if(next == null) {
+                return false;
+            }
+            String v = next.value;
+            if(i + v.length() >= word.length()) {
+                if(v.startsWith(word.substring(i, word.length()))) {
+                    return true;
+                }
+                return false;
+            }
+            else {
+                // i + v.length() < word.length()
+                if(!v.equals(word.substring(i, i+v.length()))) {
+                    return false;
+                }
+            }
+            i += v.length();
+            cur = next;
+        }
+        return true;
+    }
+}
