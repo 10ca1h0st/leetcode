56b6531b14dd5f0c812b13dbfdaa687b095bfbc4
Jiaming Wu <1114802868@qq.com>
2021-04-18 07:57:56 +0800

    做过的题，再次做，双指针，就地修改数组

--- java/No26-删除有序数组中的重复项.java
+++ java/No26-删除有序数组中的重复项.java
@@ -16,3 +16,22 @@ class Solution {
         return slow;
     }
 }
+
+// 2021-4-18 再次做题
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if(nums.length <= 1) {
+            return nums.length;
+        }
+        int slow = 1;
+        int fast = 1;
+        while(fast < nums.length) {
+            if(nums[fast] != nums[fast-1]) {
+                nums[slow] = nums[fast];
+                slow++;
+            }
+            fast++;
+        }
+        return slow;
+    }
+}
