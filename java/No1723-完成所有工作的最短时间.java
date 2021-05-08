3530cb619f4b1cea741f0af51c38848e70156d98
Jiaming Wu <1114802868@qq.com>
2021-05-08 09:39:27 +0800

    困难题，参考官方解答，可以使用二分＋dfs的方法，也可以使用动态规划

---
+++ java/No1723-完成所有工作的最短时间.java
@@ -0,0 +1,56 @@
+class Solution {
+    public int minimumTimeRequired(int[] jobs, int k) {
+        Arrays.sort(jobs);
+        int low = 0, high = jobs.length - 1;
+        while (low < high) {
+            int temp = jobs[low];
+            jobs[low] = jobs[high];
+            jobs[high] = temp;
+            low++;
+            high--;
+        }
+        int l = jobs[0], r = Arrays.stream(jobs).sum();
+        while (l < r) {
+            int mid = (l + r) >> 1;
+            if (check(jobs, k, mid)) {
+                r = mid;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return l;
+    }
+
+    public boolean check(int[] jobs, int k, int limit) {
+        int[] workloads = new int[k];
+        return backtrack(jobs, workloads, 0, limit);
+    }
+
+    public boolean backtrack(int[] jobs, int[] workloads, int i, int limit) {
+        if (i >= jobs.length) {
+            return true;
+        }
+        int cur = jobs[i];
+        for (int j = 0; j < workloads.length; ++j) {
+            if (workloads[j] + cur <= limit) {
+                workloads[j] += cur;
+                if (backtrack(jobs, workloads, i + 1, limit)) {
+                    return true;
+                }
+                workloads[j] -= cur;
+            }
+            // 如果当前工人未被分配工作，那么下一个工人也必然未被分配工作
+            // 或者当前工作恰能使该工人的工作量达到了上限
+            // 这两种情况下我们无需尝试继续分配工作
+            if (workloads[j] == 0 || workloads[j] + cur == limit) {
+                break;
+            }
+        }
+        return false;
+    }
+}
+
+作者：LeetCode-Solution
+链接：https://leetcode-cn.com/problems/find-minimum-time-to-finish-all-jobs/solution/wan-cheng-suo-you-gong-zuo-de-zui-duan-s-hrhu/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
