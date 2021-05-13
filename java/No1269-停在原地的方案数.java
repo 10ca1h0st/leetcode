22b850b70c0c77d3062c5d3ebc80e96a3542d6c8
wujiaming <1114802868@qq.com>
2021-05-13 09:16:39 +0800

    困难题，使用动态规划，参考官方解答

---
+++ java/No1269-停在原地的方案数.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int numWays(int steps, int arrLen) {
+        final int MODULO = 1000000007;
+        int maxColumn = Math.min(arrLen - 1, steps);
+        int[][] dp = new int[steps + 1][maxColumn + 1];
+        dp[0][0] = 1;
+        for (int i = 1; i <= steps; i++) {
+            for (int j = 0; j <= maxColumn; j++) {
+                dp[i][j] = dp[i - 1][j];
+                if (j - 1 >= 0) {
+                    dp[i][j] = (dp[i][j] + dp[i - 1][j - 1]) % MODULO;
+                }
+                if (j + 1 <= maxColumn) {
+                    dp[i][j] = (dp[i][j] + dp[i - 1][j + 1]) % MODULO;
+                }
+            }
+        }
+        return dp[steps][0];
+    }
+}
+
+作者：LeetCode-Solution
+链接：https://leetcode-cn.com/problems/number-of-ways-to-stay-in-the-same-place-after-some-steps/solution/ting-zai-yuan-di-de-fang-an-shu-by-leetcode-soluti/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
