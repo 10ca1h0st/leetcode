f29d70de2d37d3ab936247d73c040ad28c8c41ad
吴佳明 <1114802868@qq.com>
2021-08-08 11:11:42 +0800

    增肌LRU的自己实现

--- java/No146-LRU 缓存机制.java
+++ java/No146-LRU 缓存机制.java
@@ -34,3 +34,93 @@ class LRUCache {
  * int param_1 = obj.get(key);
  * obj.put(key,value);
  */
+
+
+// 自己实现
+class LRUCache {
+    int cap;
+    LinkedHashMap<Integer, Integer> cache = new LinkedHashMap<>();
+    public LRUCache(int capacity) { 
+        this.cap = capacity;
+    }
+
+    public int get(int key) {
+        if (!cache.containsKey(key)) {
+            return -1;
+        }
+        // 将 key 变为最近使用
+        makeRecently(key);
+        return cache.get(key);
+    }
+
+    public void put(int key, int val) {
+        if (cache.containsKey(key)) {
+            // 修改 key 的值
+            cache.put(key, val);
+            // 将 key 变为最近使用
+            makeRecently(key);
+            return;
+        }
+
+        if (cache.size() >= this.cap) {
+            // 链表头部就是最久未使用的 key
+            int oldestKey = cache.keySet().iterator().next();
+            cache.remove(oldestKey);
+        }
+        // 将新的 key 添加链表尾部
+        cache.put(key, val);
+    }
+
+    private void makeRecently(int key) {
+        int val = cache.get(key);
+        // 删除 key，重新插入到队尾
+        cache.remove(key);
+        cache.put(key, val);
+    }
+}
+
+class DoubleList {  
+    // 头尾虚节点
+    private Node head, tail;  
+    // 链表元素数
+    private int size;
+
+    public DoubleList() {
+        // 初始化双向链表的数据
+        head = new Node(0, 0);
+        tail = new Node(0, 0);
+        head.next = tail;
+        tail.prev = head;
+        size = 0;
+    }
+
+    // 在链表尾部添加节点 x，时间 O(1)
+    public void addLast(Node x) {
+        x.prev = tail.prev;
+        x.next = tail;
+        tail.prev.next = x;
+        tail.prev = x;
+        size++;
+    }
+
+    // 删除链表中的 x 节点（x 一定存在）
+    // 由于是双链表且给的是目标 Node 节点，时间 O(1)
+    public void remove(Node x) {
+        x.prev.next = x.next;
+        x.next.prev = x.prev;
+        size--;
+    }
+
+    // 删除链表中第一个节点，并返回该节点，时间 O(1)
+    public Node removeFirst() {
+        if (head.next == tail)
+            return null;
+        Node first = head.next;
+        remove(first);
+        return first;
+    }
+
+    // 返回链表长度，时间 O(1)
+    public int size() { return size; }
+
+}
