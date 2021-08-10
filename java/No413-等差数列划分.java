14e5251455a0ece2d65dcb8c40dffe34be6476ca
wujiaming.wjm <wujiaming.wjm@alibaba-inc.com>
2021-08-10 08:04:35 +0800

    动态规划，dp[i]表示以nums[i]结尾的等差子数组的个数

---
+++ java/No413-等差数列划分.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int numberOfArithmeticSlices(int[] nums) {
+        if (nums.length < 3) {
+            return 0;
+        }
+        int[] dp = new int[nums.length];
+        int ans = 0;
+        for (int i = 2; i < nums.length; i++) {
+            if (nums[i]-nums[i-1] == nums[i-1]-nums[i-2]) {
+                dp[i] = dp[i-1] + 1;
+            } else {
+                dp[i] = 0;
+            }
+            ans += dp[i];
+        }
+        return ans;
+    }
+}
