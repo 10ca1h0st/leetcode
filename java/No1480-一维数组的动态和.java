8ff2d04cdc48f1a72ffa618c55f9a60ac13ee924
wujiaming.wjm <wujiaming.wjm@alibaba-inc.com>
2021-08-28 01:31:00 +0800

    简单题 前缀和

---
+++ java/No1480-一维数组的动态和.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] runningSum(int[] nums) {
+        int[] prefixSum = new int[nums.length];
+        prefixSum[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            prefixSum[i] = prefixSum[i-1] + nums[i];
+        }
+        return prefixSum;
+    }
+}
