69c113c4320a62cc9c3c88bd535336dbfd57a0fc
Jiaming Wu <1114802868@qq.com>
2021-09-06 07:08:33 +0800

    二分查找

---
+++ java/No704-二分查找.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        int mid = -1;
+        while (left <= right) {
+            mid = left + (right-left)/2;
+            if (nums[mid] < target) {
+                left = mid+1;
+                // left++;
+            } else if (nums[mid] > target) {
+                right = mid-1;
+                // right--;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
