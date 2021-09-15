392b02049ca70235fc5159488330f5daa4ac3d4c
Jiaming Wu <1114802868@qq.com>
2021-09-15 12:36:40 +0800

    加入双栈实现的计算器解法

--- java/utility/加减乘除和括号的计算器.java
+++ java/utility/加减乘除和括号的计算器.java
@@ -1,25 +1,27 @@
 // 双栈实现的计算器
 class Solution {
+    Map<Character, Integer> map = new HashMap<>(){{
+        put('-', 1);
+        put('+', 1);
+        put('*', 2);
+        put('/', 2);
+        put('%', 2);
+        put('^', 3);
+    }};
     public int calculate(String s) {
-        // 存放所有的数字
-        Deque<Integer> nums = new ArrayDeque<>();
-        // 为了防止第一个数为负数，先往 nums 加个 0
-        nums.addLast(0);
-        // 将所有的空格去掉
         s = s.replaceAll(" ", "");
-        // 存放所有的操作，包括 +/-
-        Deque<Character> ops = new ArrayDeque<>();
-        int n = s.length();
         char[] cs = s.toCharArray();
+        int n = s.length();
+        Deque<Integer> nums = new ArrayDeque<>();
+        nums.addLast(0);
+        Deque<Character> ops = new ArrayDeque<>();
         for (int i = 0; i < n; i++) {
             char c = cs[i];
             if (c == '(') {
                 ops.addLast(c);
             } else if (c == ')') {
-                // 计算到最近一个左括号为止
                 while (!ops.isEmpty()) {
-                    char op = ops.peekLast();
-                    if (op != '(') {
+                    if (ops.peekLast() != '(') {
                         calc(nums, ops);
                     } else {
                         ops.pollLast();
@@ -27,24 +29,29 @@ class Solution {
                     }
                 }
             } else {
-                if (isNum(c)) {
+                if (isNumber(c)) {
                     int u = 0;
                     int j = i;
-                    // 将从 i 位置开始后面的连续数字整体取出，加入 nums
-                    while (j < n && isNum(cs[j])) u = u * 10 + (int)(cs[j++] - '0');
+                    while (j < n && isNumber(cs[j])) u = u * 10 + (cs[j++] - '0');
                     nums.addLast(u);
                     i = j - 1;
                 } else {
                     if (i > 0 && (cs[i - 1] == '(' || cs[i - 1] == '+' || cs[i - 1] == '-')) {
                         nums.addLast(0);
                     }
-                    // 有一个新操作要入栈时，先把栈内可以算的都算了
-                    while (!ops.isEmpty() && ops.peekLast() != '(') calc(nums, ops);
+                    while (!ops.isEmpty() && ops.peekLast() != '(') {
+                        char prev = ops.peekLast();
+                        if (map.get(prev) >= map.get(c)) {
+                            calc(nums, ops);
+                        } else {
+                            break;
+                        }
+                    }
                     ops.addLast(c);
                 }
             }
         }
-        while (!ops.isEmpty()) calc(nums, ops);
+        while (!ops.isEmpty() && ops.peekLast() != '(') calc(nums, ops);
         return nums.peekLast();
     }
     void calc(Deque<Integer> nums, Deque<Character> ops) {
@@ -52,9 +59,23 @@ class Solution {
         if (ops.isEmpty()) return;
         int b = nums.pollLast(), a = nums.pollLast();
         char op = ops.pollLast();
-        nums.addLast(op == '+' ? a + b : a - b);
+        int ans = 0;
+        if (op == '+') {
+            ans = a + b;
+        } else if (op == '-') {
+            ans = a - b;
+        } else if (op == '*') {
+            ans = a * b;
+        } else if (op == '/') {
+            ans = a / b;
+        } else if (op == '^') {
+            ans = (int)Math.pow(a, b);
+        } else if (op == '%') {
+            ans = a % b;
+        }
+        nums.addLast(ans);
     }
-    boolean isNum(char c) {
+    boolean isNumber(char c) {
         return Character.isDigit(c);
     }
 }
