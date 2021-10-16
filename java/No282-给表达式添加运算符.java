01441a0cbb5933573a2bce2591df7beb8983f37b
吴佳明 <1114802868@qq.com>
2021-10-16 09:23:55 +0800

    困难题 参考官方题解

---
+++ java/No282-给表达式添加运算符.java
@@ -0,0 +1,51 @@
+class Solution {
+    int n;
+    String num;
+    int target;
+    List<String> ans;
+
+    public List<String> addOperators(String num, int target) {
+        this.n = num.length();
+        this.num = num;
+        this.target = target;
+        this.ans = new ArrayList<String>();
+        StringBuffer expr = new StringBuffer();
+        backtrack(expr, 0, 0, 0);
+        return ans;
+    }
+
+    public void backtrack(StringBuffer expr, int i, long res, long mul) {
+        if (i == n) {
+            if (res == target) {
+                ans.add(expr.toString());
+            }
+            return;
+        }
+        int signIndex = expr.length();
+        if (i > 0) {
+            expr.append(0); // 占位，下面填充符号
+        }
+        long val = 0;
+        // 枚举截取的数字长度（取多少位），注意数字可以是单个 0 但不能有前导零
+        for (int j = i; j < n && (j == i || num.charAt(i) != '0'); ++j) {
+            val = val * 10 + num.charAt(j) - '0';
+            expr.append(num.charAt(j));
+            if (i == 0) { // 表达式开头不能添加符号
+                backtrack(expr, j + 1, val, val);
+            } else { // 枚举符号
+                expr.setCharAt(signIndex, '+');
+                backtrack(expr, j + 1, res + val, val);
+                expr.setCharAt(signIndex, '-');
+                backtrack(expr, j + 1, res - val, -val);
+                expr.setCharAt(signIndex, '*');
+                backtrack(expr, j + 1, res - mul + mul * val, mul * val);
+            }
+        }
+        expr.setLength(signIndex);
+    }
+}
+
+作者：LeetCode-Solution
+链接：https://leetcode-cn.com/problems/expression-add-operators/solution/gei-biao-da-shi-tian-jia-yun-suan-fu-by-2o1s7/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
