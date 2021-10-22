31a805301bc43ee13c5c8bd015dfb843b0be05a4
Jiaming Wu <1114802868@qq.com>
2021-10-22 08:31:31 +0800

    中等题

---
+++ java/No229-求众数II.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        HashMap<Integer, Integer> cnt = new HashMap<Integer, Integer>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (cnt.containsKey(nums[i])) {
+                cnt.put(nums[i], cnt.get(nums[i]) + 1);
+            } else {
+                cnt.put(nums[i], 1);
+            }
+        }
+        List<Integer> ans = new ArrayList<>();
+        for (int x : cnt.keySet()) {
+            if (cnt.get(x) > nums.length / 3) {
+                ans.add(x);
+            }
+        }
+   
+        return ans;
+    }
+}
