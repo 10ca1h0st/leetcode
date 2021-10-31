7ceecc0064401b92c8f071269d8474daf6a88048
Jiaming Wu <1114802868@qq.com>
2021-10-31 08:29:01 +0800

    简单题

---
+++ java/No500-键盘行.java
@@ -0,0 +1,24 @@
+class Solution {    
+    public String[] findWords(String[] words) {
+        List<String> list = new ArrayList<String>();
+        String rowIdx = "12210111011122000010020202";
+        for (String word : words) {
+            boolean isValid = true;
+            char idx = rowIdx.charAt(Character.toLowerCase(word.charAt(0)) - 'a');
+            for (int i = 1; i < word.length(); ++i) {
+                if (rowIdx.charAt(Character.toLowerCase(word.charAt(i)) - 'a') != idx) {
+                    isValid = false;
+                    break;
+                }
+            }
+            if (isValid) {
+                list.add(word);
+            }
+        }
+        String[] ans = new String[list.size()];
+        for (int i = 0; i < list.size(); ++i) {
+            ans[i] = list.get(i);
+        }
+        return ans;
+    }
+}
