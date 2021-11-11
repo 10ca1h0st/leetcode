114e1a000f1fcf86653817c78422a21b0a791193
Jiaming Wu <1114802868@qq.com>
2021-11-11 08:55:04 +0800

    困难题 dp 优化

---
+++ java/No629-K个逆序对数组.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int kInversePairs(int n, int k) {
+        final int MOD = 1000000007;
+        int[][] f = new int[2][k + 1];
+        f[0][0] = 1;
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 0; j <= k; ++j) {
+                int cur = i & 1, prev = cur ^ 1;
+                f[cur][j] = (j - 1 >= 0 ? f[cur][j - 1] : 0) - (j - i >= 0 ? f[prev][j - i] : 0) + f[prev][j];
+                if (f[cur][j] >= MOD) {
+                    f[cur][j] -= MOD;
+                } else if (f[cur][j] < 0) {
+                    f[cur][j] += MOD;
+                }
+            }
+        }
+        return f[n & 1][k];
+    }
+}
