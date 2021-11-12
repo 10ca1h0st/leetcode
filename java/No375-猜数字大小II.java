1e00cdd6f6eba371e0cb30b62700728c59d923f4
Jiaming Wu <1114802868@qq.com>
2021-11-12 08:30:13 +0800

    动态规划

---
+++ java/No375-猜数字大小II.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int getMoneyAmount(int n) {
+        int[][] f = new int[n + 1][n + 1];
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = i + 1; j <= n; j++) {
+                int minCost = Integer.MAX_VALUE;
+                for (int k = i; k < j; k++) {
+                    int cost = k + Math.max(f[i][k - 1], f[k + 1][j]);
+                    minCost = Math.min(minCost, cost);
+                }
+                f[i][j] = minCost;
+            }
+        }
+        return f[1][n];
+    }
+}
