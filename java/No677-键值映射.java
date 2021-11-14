b1ae8ea23856db089e5d375b663649119e863a55
Jiaming Wu <1114802868@qq.com>
2021-11-14 09:43:31 +0800

    字典树

---
+++ java/No677-键值映射.java
@@ -0,0 +1,43 @@
+class MapSum {
+    class TrieNode {
+        int val = 0;
+        TrieNode[] next = new TrieNode[26];
+    }
+
+    TrieNode root;
+    Map<String, Integer> map;
+
+    public MapSum() {
+        root = new TrieNode();
+        map = new HashMap<>();
+    }
+    
+    public void insert(String key, int val) {        
+        int delta = val - map.getOrDefault(key, 0);
+        map.put(key, val);
+        TrieNode node = root;
+        for (char c : key.toCharArray()) {
+            if (node.next[c - 'a'] == null) {
+                node.next[c - 'a'] = new TrieNode();
+            }
+            node = node.next[c - 'a'];
+            node.val += delta;
+        }
+    }
+    
+    public int sum(String prefix) {
+        TrieNode node = root;
+        for (char c : prefix.toCharArray()) {
+            if (node.next[c - 'a'] == null) {
+                return 0;
+            }
+            node = node.next[c - 'a'];
+        }
+        return node.val;
+    }
+}
+
+作者：LeetCode-Solution
+链接：https://leetcode-cn.com/problems/map-sum-pairs/solution/jian-zhi-ying-she-by-leetcode-solution-j4xy/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
