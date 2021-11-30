a39eaadc8e49dce690407330a9bd4b3d1c77a4a5
Jiaming Wu <1114802868@qq.com>
2021-11-30 08:44:29 +0800

    中等题 模拟

---
+++ java/No400-第N位数字.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int findNthDigit(int n) {
+        int low = 1, high = 9;
+        while (low < high) {
+            int mid = (high - low) / 2 + low;
+            if (totalDigits(mid) < n) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        int d = low;
+        int prevDigits = totalDigits(d - 1);
+        int index = n - prevDigits - 1;
+        int start = (int) Math.pow(10, d - 1);
+        int num = start + index / d;
+        int digitIndex = index % d;
+        int digit = (num / (int) (Math.pow(10, d - digitIndex - 1))) % 10;
+        return digit;
+    }
+
+    public int totalDigits(int length) {
+        int digits = 0;
+        int curLength = 1, curCount = 9;
+        while (curLength <= length) {
+            digits += curLength * curCount;
+            curLength++;
+            curCount *= 10;
+        }
+        return digits;
+    }
+}
+
+作者：LeetCode-Solution
+链接：https://leetcode-cn.com/problems/nth-digit/solution/di-n-wei-shu-zi-by-leetcode-solution-mdl2/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
